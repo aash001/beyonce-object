00097965c40cf7ced2351afba5600a3e0afc19cf
aampinar <98187277+aampinar@users.noreply.github.com>
2022-03-31 18:19:33 -0400

    Solution for 19 & 20

--- index.js
+++ index.js
@@ -212,14 +212,47 @@ function uniqueHairstyles() {
 
 // 19. Return an object where the properties are song names and the value is an object which contains that song's fierceness and the average fierceness for all songs
 function songFiercenessByName() {
-
+  return beyonceHash.hits.map(hit => {
+      return {
+        title: hit.title,
+        fierceness: hit.fierceness,
+        averageFierceness: hitFiercenessAverage()
+      }
+    })
+    .reduce((singles, hit) => {
+      const fierceness = {
+        ...singles,
+        [hit.title]: hit,
+      }
+      delete hit.title;
+      return fierceness;
+    }, {})
+}
+// 20. Return an object where the properties are movie names and the value is an object which contains that movie's rating and the average rating for all movies
+function movieRatingsByName() {
+  return beyonceHash.movies.map(movie => {
+      return {
+        title: movie.title,
+        rating: movie.rating,
+        average: ratingAverage()
+      }
+    })
+    .reduce((box, movie) => {
+      const rating = {
+        ...box,
+        [movie.title]: movie,
+      }
+      delete movie.title;
+      return rating;
+    }, {})
 }
 
-// 20. Return an object where the properties are movie names and the value is an object which contains that movie's rating and the average rating for all movies
-function movieRatingsByName() {}
 
 // 21. Return an object with Beyonce's hairstyles as the keys and a tally of each hairstyle, eg. `{ "blonde": 3, ... }`
 function hairStyleFrequency() {}
+
+
+//Testing Zone
 
 
 console.log(hitFiercenessSum())
@@ -228,3 +261,5 @@ console.log(ratingSum());
 console.log(ratingAverage());
 console.log(hitDancerSum());
 console.log(uniqueHairstyles());
+console.log(songFiercenessByName());
+console.log(movieRatingsByName());
